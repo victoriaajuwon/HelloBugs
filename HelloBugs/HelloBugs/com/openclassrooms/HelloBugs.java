e0709549862470772076d4670d1dd800d015938a
victoriaajuwon <122409743+victoriaajuwon@users.noreply.github.com>
2023-02-05 15:22:52 -0500

    Initial commit

---
+++ HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -0,0 +1,13 @@
+package com.openclassrooms;
+
+public clas HelloBugs {
+
+	public static void main(String{}args) {
+		
+		for(i=0;i>=0;i++) {
+		System.out.println("Hello buggy code!"):
+		}
+				
+	}
+
+}
